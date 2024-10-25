f475cf46433ed7055b078346f782a78e4bc46bbc
Davide Stenner <48441753+DavideStenner@users.noreply.github.com>
2024-10-25 17:37:22 +0200

    xgb metric on ordinal encoding

--- src/model/metric/official_metric.py
+++ src/model/metric/official_metric.py
@@ -1,5 +1,6 @@
 import numpy as np
 import lightgbm as lgb
+import xgboost as xgb
 
 from typing import Tuple
 from sklearn.metrics import cohen_kappa_score
@@ -13,3 +14,9 @@ def lgb_quadratic_kappa(y_pred: np.ndarray, data: lgb.Dataset) -> Tuple[str, flo
     y_true = data.get_label()
 
     return 'q_kappa', quadratic_weighted_kappa(y_true=y_true, y_pred=rounded_y_pred), True
+
+def xgb_quadratic_kappa(y_pred: np.ndarray, eval_data: xgb.DMatrix) -> Tuple[str, float]:
+    y_true = eval_data.get_label().reshape(y_pred.shape).cumsum(axis=1).argmax(axis=1)
+    y_pred = (y_pred>0.5).cumsum(axis=1).argmax(axis=1)
+
+    return 'q_kappa', quadratic_weighted_kappa(y_true=y_true, y_pred=y_pred)
