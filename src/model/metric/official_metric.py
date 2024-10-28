85e9197eafc54eba33b04586ed2f5db76975cc3a
Davide Stenner <48441753+DavideStenner@users.noreply.github.com>
2024-10-28 10:06:36 +0100

    added function for ordinal kappa

--- src/model/metric/official_metric.py
+++ src/model/metric/official_metric.py
@@ -15,8 +15,13 @@ def lgb_quadratic_kappa(y_pred: np.ndarray, data: lgb.Dataset) -> Tuple[str, flo
 
     return 'q_kappa', quadratic_weighted_kappa(y_true=y_true, y_pred=rounded_y_pred), True
 
-def xgb_quadratic_kappa(y_pred: np.ndarray, eval_data: xgb.DMatrix) -> Tuple[str, float]:
-    y_true = eval_data.get_label().reshape(y_pred.shape).cumsum(axis=1).argmax(axis=1)
+def xgb_ordinal_kappa(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    y_true = y_true.cumsum(axis=1).argmax(axis=1)
     y_pred = (y_pred>0.5).cumsum(axis=1).argmax(axis=1)
+    return quadratic_weighted_kappa(y_true=y_true, y_pred=y_pred)
 
-    return 'q_kappa', quadratic_weighted_kappa(y_true=y_true, y_pred=y_pred)
+def xgb_quadratic_kappa(y_pred: np.ndarray, eval_data: xgb.DMatrix) -> Tuple[str, float]:
+    y_true = eval_data.get_label().reshape(y_pred.shape)
+
+    kappa_score = xgb_ordinal_kappa(y_true=y_true, y_pred=y_pred)
+    return 'q_kappa', kappa_score
