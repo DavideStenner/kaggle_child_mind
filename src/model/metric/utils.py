57cb051d767283c215c3f1c062f8a752c4c89b17
Davide Stenner <48441753+DavideStenner@users.noreply.github.com>
2024-10-30 12:30:54 +0100

    utilities for total sii

--- src/model/metric/utils.py
+++ src/model/metric/utils.py
@@ -6,3 +6,18 @@ def get_ordinal_target(target_array: np.ndarray, num_target: int) -> np.ndarray:
         ordinal_train_target[i, :(target_array[i]+1)] = 1
         
     return ordinal_train_target
+
+def total_to_sii(vector_total: np.ndarray) -> np.ndarray:
+    vector_total = (
+        np.where(
+            vector_total <=30, 0,
+            np.where(
+                vector_total <=49, 1,
+                    np.where(
+                        vector_total <=79 , 2, 
+                        3
+                    )
+                )
+        )
+    )
+    return vector_total
