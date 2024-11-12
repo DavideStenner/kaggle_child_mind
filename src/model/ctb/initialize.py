9621e8ec7e822bfbdf06ec9d6370bc0d6b20128a
Davide Stenner <48441753+DavideStenner@users.noreply.github.com>
2024-11-12 09:26:37 +0100

    catboost first push

---
+++ src/model/ctb/initialize.py
@@ -0,0 +1,329 @@
+import os
+import json
+import pickle
+import logging
+
+import numpy as np
+import polars as pl
+import catboost as cb
+
+from typing import Any, Union, Dict, Tuple
+from itertools import combinations
+from src.base.model.initialize import ModelInit
+from src.utils.logging_utils import get_logger
+
+class CtbInit(ModelInit):
+    def __init__(self, 
+            experiment_name:str, 
+            params_ctb: dict[str, Any],
+            config_dict: dict[str, Any],
+            fold_name: str = 'fold_info'
+        ):
+        self.inference: bool = False
+        self.config_dict: dict[str, Any] = config_dict
+        
+        self.model_used: list[str] = ['main']
+        
+        self.model_metric_used: list[str] = {
+            target_: {'label': 'q_kappa', 'maximize': True}
+            for target_ in self.model_used
+        }
+        self.experiment_path: str = os.path.join(
+            config_dict['PATH_EXPERIMENT'],
+            experiment_name,
+        )
+        self.experiment_model_path: str = os.path.join(
+            self.experiment_path, '{model_type}'
+        )
+        
+        self.experiment_path_dict: dict[str, str] = {
+            'feature_importance': os.path.join(
+                self.experiment_model_path, 'insight'
+            ),
+            'insight': os.path.join(
+                self.experiment_model_path, 'insight'
+            ),
+            'training': os.path.join(
+                self.experiment_model_path, 'training'
+            ),
+            'shap': os.path.join(
+                self.experiment_model_path, 'shap'
+            ),
+            'model': os.path.join(
+                self.experiment_model_path, 'model'
+            ),
+            'pseudo_labeling': os.path.join(
+                self.experiment_model_path, 'pseudo_labeling'
+            )
+        }
+
+        self.n_fold: int = config_dict['N_FOLD']
+        
+        self.fold_name: str = fold_name
+
+        self.params_ctb: dict[str, Any] = params_ctb
+        
+        self.feature_list: list[str] = []
+        self.original_path_gold: str = self.config_dict['PATH_GOLD_DATA']
+
+        self.get_categorical_columns()
+        self.initialize_model_utils()
+        self.get_model_file_name_dict()
+        self.get_col_list()
+    
+    @property
+    def name(self) -> str:
+        return 'catboost'
+    
+    def get_col_list(self) -> None:
+
+        self.useless_col_list: list[str] = (
+            self.config_dict['COLUMN_INFO']['SPECIAL_COLUMNS'] +
+            self.config_dict['COLUMN_INFO']['ORIGINAL_TARGET_LIST'] +
+            [self.id_row] +
+            [self.config_dict['COLUMN_INFO']['TARGET']] +
+            [
+                'fold_info', 'current_fold'
+            ]
+        )
+        self.target_col: str = self.config_dict['COLUMN_INFO']['TARGET']
+        
+
+    def initialize_logger(self) -> None:
+        self.training_logger: logging.Logger = get_logger(
+            file_name='training_log.txt', path_logger=self.experiment_path
+        )
+        
+    def initialize_model_utils(self) -> None:
+        self.id_row: str = self.config_dict['ID_COL']
+
+        self.feature_precision: str = 'float64'
+        self.target_precision: str = 'float64'
+
+        for model_type in self.model_used:
+            setattr(
+                self, f'model_{model_type}_list', [] 
+            )
+            
+            setattr(
+                self, f'progress_{model_type}_list', [] 
+            )
+
+    def set_postprocess_utils(self) -> None:
+        max_range: int = (
+            self.config_dict['COLUMN_INFO']['TARGET_N_UNIQUE'] if
+            self.config_dict['COLUMN_INFO']['TARGET'] == 'sii'
+            else 80
+        )
+        total_grid = np.linspace(0, max_range-1, 50)
+
+        self.list_treshold_value: list[list[float]] = list(
+            combinations(total_grid, 3)
+        )
+                                
+    def get_categorical_columns(self) -> None:
+        #load all possible categorical feature
+        self.categorical_col_list: list[str] = (
+            self.config_dict['COLUMN_INFO']['STRING_FEATURE'] +
+            self.config_dict['COLUMN_INFO']['CATEGORICAL_FEATURE']
+        )
+        
+    def create_experiment_structure(self) -> None:
+        if not os.path.isdir(self.experiment_path):
+            os.makedirs(self.experiment_path)
+            
+        for model_type in self.model_used:
+            if not os.path.isdir(self.experiment_model_path.format(model_type=model_type)):
+                os.makedirs(self.experiment_model_path.format(model_type=model_type))
+            
+            for dir_path_format in self.experiment_path_dict.values():
+                dir_path: str = dir_path_format.format(model_type=model_type)
+                if not os.path.isdir(dir_path):
+                    os.makedirs(dir_path)
+
+    def load_model(self, target: str) -> None: 
+        self.load_used_feature(target=target)
+        self.load_used_categorical_feature(target=target)
+        self.load_best_result(target=target)
+        self.load_params()
+        
+        self.load_pickle_model_list(type_model=target)
+    
+    def get_model_file_name_dict(self) -> None:
+        self.model_file_name_dict: dict[str, str] =  {
+            'progress_list': {
+                model_type: f'progress_list.pkl'
+                for model_type in self.model_used
+            },
+            'best_result': {
+                model_type: f'best_result.txt'
+                for model_type in self.model_used
+            },
+            'model_pickle_list': {
+                model_type: f'model_list.pkl'
+                for model_type in self.model_used
+            },
+            'model_list': {
+                model_type: 'lgb_{fold_}.txt'
+                for model_type in self.model_used
+            }
+        }
+
+    def load_best_pseudo_result(self) -> dict[str, any]:
+        with open(
+            os.path.join(
+                self.experiment_path,
+                'best_pseudo_result.json'
+            ), 'r'
+        ) as file:
+            best_result = json.load(file)
+            
+        return best_result
+
+    def save_best_pseudo_result(self, pseudo_result: dict[str, any]) -> None:
+        with open(
+            os.path.join(
+                self.experiment_path,
+                'best_pseudo_result.json'
+            ), 'w'
+        ) as file:
+            json.dump(pseudo_result, file)
+
+    def save_progress_list(self, progress_list: list, model_type: str) -> None:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                self.model_file_name_dict['progress_list'][model_type]
+            ), 'wb'
+        ) as file:
+            pickle.dump(progress_list, file)
+
+    def load_progress_list(self, model_type: str) -> list:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                self.model_file_name_dict['progress_list'][model_type]
+            ), 'rb'
+        ) as file:
+            progress_list = pickle.load(file)
+            
+        return progress_list
+    
+    def save_params(self) -> None:
+        with open(
+            os.path.join(
+                self.experiment_path,
+                'params_ctb.json'
+            ), 'w'
+        ) as file:
+            json.dump(self.params_ctb, file)
+    
+    def load_params(self) -> None:
+        with open(
+            os.path.join(
+                self.experiment_path,
+                'params_ctb.json'
+            ), 'r'
+        ) as file:
+            self.params_ctb = json.load(file)
+    
+    def save_best_result(self, best_result: dict[str, Union[int, float]], model_type: str) -> None:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                self.model_file_name_dict['best_result'][model_type]
+            ), 'w'
+        ) as file:
+            json.dump(best_result, file)
+        
+    def load_best_result(self, model_type: str) -> dict[str, Union[int, float]]:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                self.model_file_name_dict['best_result'][model_type]
+            ), 'r'
+        ) as file:
+            best_result = json.load(file)
+            
+        return best_result
+    
+    def save_pickle_model_list(self, model_list: list[cb.CatBoost], model_type: str) -> None:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                'model',
+                self.model_file_name_dict['model_pickle_list'][model_type]
+            ), 'wb'
+        ) as file:
+            pickle.dump(model_list, file)
+    
+    def load_pickle_model_list(self, model_type: str) -> list[cb.CatBoost]:
+        with open(
+            os.path.join(
+                self.experiment_path_dict['model'].format(model_type=model_type),
+                self.model_file_name_dict['model_pickle_list'][model_type]
+            ), 'rb'
+        ) as file:
+            model_list = pickle.load(file)
+    
+        return model_list
+    
+    def load_model_list(self, model_type: str, file_name: str) -> list[cb.CatBoost]:
+        
+        return [
+            cb.CatBoost(
+                params=self.params_ctb,
+            ).load_model(
+                fname=os.path.join(
+                    self.experiment_path_dict['model'].format(model_type=model_type),
+                    file_name.format(fold_=fold_)
+                )
+            )
+            for fold_ in range(self.n_fold)
+        ]    
+            
+    def save_used_feature(self, model_type: str, feature_list: list[str]) -> None:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                'used_feature.txt'
+            ), 'w'
+        ) as file:
+            json.dump(
+                {
+                    'feature_model': feature_list
+                }, 
+                file
+            )
+    
+    def load_used_categorical_feature(self, model_type: str) -> None:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                'used_categorical_feature.txt'
+            ), 'r'
+        ) as file:
+            self.categorical_col_list = json.load(file)['categorical_feature']
+            
+    def save_used_categorical_feature(self, model_type: str) -> None:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                'used_categorical_feature.txt'
+            ), 'w'
+        ) as file:
+            json.dump(
+                {
+                    'categorical_feature': self.categorical_col_list
+                }, 
+                file
+            )
+
+    def load_used_feature(self, model_type: str) -> None:
+        with open(
+            os.path.join(
+                self.experiment_model_path.format(model_type=model_type),
+                'used_feature.txt'
+            ), 'r'
+        ) as file:
+            self.feature_list = json.load(file)['feature_model']
---
+++ src/model/ctb/training.py
@@ -0,0 +1,342 @@
+import os
+import gc
+import numpy as np
+import pandas as pd
+import polars as pl
+import catboost as cb
+
+from functools import partial
+from typing import Tuple, Dict
+
+from src.base.model.training import ModelTrain
+from src.model.ctb.initialize import CtbInit
+from src.model.metric.official_metric import lgb_quadratic_kappa
+
+class CtbTrainer(ModelTrain, CtbInit):
+    def _init_train(self) -> None:
+        data = pl.scan_parquet(
+            os.path.join(
+                self.config_dict['PATH_GOLD_DATA'],
+                'data.parquet'
+            )
+        )
+        
+        self.feature_list = [
+            col for col in data.collect_schema().names()
+            if col not in self.useless_col_list + [self.target_col]
+        ]
+        self.categorical_col_list: list[str] = [
+            col for col in self.categorical_col_list
+            if col in self.feature_list
+        ]
+        self.training_logger.info(f'Using {len(self.categorical_col_list)} categorical features')
+
+    def access_fold(self, fold_: int) -> pl.LazyFrame:
+        fold_data = (
+            pl.scan_parquet(
+                os.path.join(
+                    self.config_dict['PATH_GOLD_DATA'],
+                    f'data.parquet'
+                )
+            ).with_columns(
+                (
+                    pl.col('fold_info').str.split(', ')
+                    .list.get(fold_).alias('current_fold')
+                )
+            )
+        )
+        return fold_data
+
+
+    def train_target(self, fold_: int, model_type: str) -> None:
+        #classification metric
+        params_lgb = self.params_ctb
+        progress = {}
+
+        train_matrix, test_matrix = self.get_dataset(fold_=fold_)
+
+        self.training_logger.info(f'Start {model_type} training')
+        model = cb.CatBoostRegressor(
+            **self.params_ctb,
+            allow_writing_files=False, use_best_model=False,
+            # eval_metric=CTBGiniStability(test_data=test_metric_df, train_data=train_metric_df),
+        )
+        model.fit(
+            train_matrix, 
+            eval_set=test_matrix, 
+            metric_period=100,
+        )
+
+        setattr(
+            self, f"model_{model_type}_list",
+            (
+                getattr(
+                    self, f"model_{model_type}_list"
+                ) +
+                [model]
+            )
+        )
+        setattr(
+            self, f"progress_{model_type}_list",
+            (
+                getattr(
+                    self, f"progress_{model_type}_list"
+                ) +
+                [progress]
+            )
+        )
+
+        del train_matrix, test_matrix
+        
+        _ = gc.collect()
+
+    def __check_validity_train_dataset(self) -> None:
+        NUMERIC_POLARS_DTYPES: list[pl.DataType] = [
+            pl.Int8, pl.Int16, pl.Int32, pl.Int64, 
+            pl.UInt8, pl.UInt16, pl.UInt32, pl.UInt64,
+            pl.Float32, pl.Float64, 
+        ]
+
+        data = self.access_fold(fold_=0)
+        feature_dataset = data.select(self.feature_list).clone()
+        
+        original_columns = feature_dataset.collect_schema().names()
+        only_numerical = feature_dataset.select(pl.all()).select(pl.col(NUMERIC_POLARS_DTYPES)).collect_schema().names()
+        
+        #no timestamp or string
+        assert set(original_columns) == set(only_numerical)
+        
+        sum_infinite_value: int = (
+            feature_dataset
+            .select(pl.all().is_infinite().sum())
+            .select(pl.sum_horizontal(pl.all()).alias('any_infinite'))
+            .collect()
+            .item()
+        )
+        assert sum_infinite_value == 0
+        
+    def get_dataset(self, fold_: int) -> Tuple[cb.Pool]:
+        fold_data = self.access_fold(fold_=fold_)
+                    
+        train_filtered = fold_data.filter(
+            (pl.col('current_fold') == 't')
+        )
+        test_filtered = fold_data.filter(
+            (pl.col('current_fold') == 'v')
+        )
+        
+        if not self.config_dict['ONLINE']:
+            assert len(
+                set(
+                    train_filtered.select(self.id_row).unique().collect().to_series().to_list()
+                ).intersection(
+                    test_filtered.select(self.id_row).unique().collect().to_series().to_list()
+                )
+            ) == 0
+                        
+        train_matrix = cb.Pool(
+            data=train_filtered.select(self.feature_list).collect().to_pandas().to_numpy(self.feature_precision),
+            label=train_filtered.select(self.target_col).collect().to_pandas().to_numpy(self.target_precision),
+            feature_names=self.feature_list, cat_features=self.categorical_col_list
+        )
+        test_matrix = cb.Pool(
+            data=test_filtered.select(self.feature_list).collect().to_pandas().to_numpy(self.feature_precision),
+            label=test_filtered.select(self.target_col).collect().to_pandas().to_numpy(self.target_precision),
+            feature_names=self.feature_list, cat_features=self.categorical_col_list
+        )
+        return train_matrix, test_matrix
+
+    def train(self) -> None:
+        
+        self._init_train()
+        
+        for model_type in self.model_used:
+            
+            #save feature list locally for later
+            self.save_used_feature(model_type=model_type, feature_list=self.feature_list)
+            self.save_used_categorical_feature(model_type=model_type)
+            self.__check_validity_train_dataset()
+            
+            self.training_logger.info(f'Start {model_type} with {len(self.feature_list)} features')
+
+            for fold_ in range(self.n_fold):
+                self.training_logger.info(f'\n\nStarting fold {fold_}\n\n\n')
+                self.training_logger.info('Collecting dataset')
+        
+                self.train_target(fold_=fold_, model_type=model_type)
+            
+            self.save_model(target=model_type)
+            
+    def begin_pseudo_label(self, model_type: str) -> str:
+        assert model_type in self.model_used
+        self.training_logger.info(f'Starting pseudo labeling {model_type} experiment')
+        
+        self.load_used_feature(model_type=model_type)
+        self.load_used_categorical_feature(model_type=model_type)
+        
+        self.save_oof_pseudo_label(model_type=model_type)
+        
+        if not model_type.split('_')[-1].isnumeric():
+            new_model_name = f'{model_type}_pseudo_0'
+        else:
+            base_name: str = '_'.join(model_type.split('_')[:-1])
+            pseudo_iteration: int = int(model_type.split('_')[-1])
+            new_model_name = f'{base_name}_{pseudo_iteration+1}'
+        
+        self.model_used = [new_model_name]
+        self.model_metric_used[new_model_name] = self.model_metric_used[model_type]
+        
+        self.initialize_model_utils()
+        self.get_model_file_name_dict()
+        
+        return new_model_name
+
+    def save_oof_pseudo_label(self, model_type: str) -> None:
+        best_result_ = self.load_best_result(model_type=model_type)
+        model_list = self.load_pickle_model_list(model_type=model_type)
+        assert best_result_
+        
+        null_data = pl.read_parquet(
+            os.path.join(
+                self.original_path_gold,
+                f'data_null.parquet'
+            )
+        )
+        test_data = pl.read_parquet(
+            os.path.join(
+                self.original_path_gold,
+                f'test_data.parquet'
+            )
+        )
+        base_data_casted = (
+            pl.read_parquet(
+                os.path.join(
+                    self.original_path_gold,
+                    f'data.parquet'
+                )
+            )
+            .with_columns(pl.col(self.target_col).cast(pl.Float64))
+        )
+        list_pseudo_null_dataset: list[pl.DataFrame] = []
+        
+        if self.config_dict['ONLINE']:
+            list_pseudo_test_dataset: list[pl.DataFrame] = []
+
+        for fold_ in range(self.n_fold):
+            fold_data = (
+                pl.scan_parquet(
+                    os.path.join(
+                        self.original_path_gold,
+                        f'data.parquet'
+                    )
+                ).with_columns(
+                    (
+                        pl.col('fold_info').str.split(', ')
+                        .list.get(fold_).alias('current_fold')
+                    )
+                )
+            )
+                    
+            pseudo_train_current_fold: str = (
+                fold_data.filter(
+                    (pl.col('current_fold') == 'v')
+                )
+                .select(pl.col('fold_info').first())
+                .collect()
+                .item()
+            )
+            pseudo_train_current_fold = (
+                pseudo_train_current_fold
+                .replace('t', 'n')
+                .replace('v', 't')
+            )
+            pseudo_null = null_data.to_pandas().copy(deep=True)
+            pseudo_test = test_data.to_pandas().copy(deep=True)
+            
+            oof_prediction: np.ndarray = model_list[fold_].predict(
+                 (
+                    pseudo_null[self.feature_list]
+                    .to_numpy(self.feature_precision)
+                ),
+                num_iteration=best_result_['best_epoch']
+            )
+            pseudo_null[self.target_col] = oof_prediction
+            pseudo_null['fold_info'] = pseudo_train_current_fold
+            
+            list_pseudo_null_dataset.append(
+                pl.from_dataframe(pseudo_null).with_columns(pl.col(self.target_col).cast(pl.Float64)),
+            )
+
+            if self.config_dict['ONLINE']:
+                oof_prediction_test: np.ndarray = model_list[fold_].predict(
+                    (
+                        pseudo_test[self.feature_list]
+                        .to_numpy(self.feature_precision)
+                    ),
+                    num_iteration=best_result_['best_epoch']
+                )
+                pseudo_test[self.target_col] = oof_prediction_test
+                pseudo_test['fold_info'] = pseudo_train_current_fold
+
+                list_pseudo_test_dataset.append(
+                    pl.from_dataframe(pseudo_test).with_columns(pl.col(self.target_col).cast(pl.Float64))
+                )
+            
+        #concat and select common columns
+        if self.config_dict['ONLINE']:
+            pseudo_test_dataset = pl.concat(list_pseudo_test_dataset)
+            keep_col_list: list[str] = pseudo_test_dataset.collect_schema().names()
+        else:
+            keep_col_list: list[str] = base_data_casted.collect_schema().names()
+            
+        pseudo_null_dataset = (
+            pl.concat(list_pseudo_null_dataset)
+            .select(keep_col_list)
+        )
+        base_data_casted = (
+            base_data_casted
+            .select(keep_col_list)
+        )
+        
+        if self.config_dict['ONLINE']:
+            pseudo_dataset = (
+                pl.concat(
+                    [
+                        pseudo_null_dataset.cast(base_data_casted.collect_schema()), 
+                        pseudo_test_dataset.cast(base_data_casted.collect_schema())
+                    ]
+                )
+            )
+        else:
+            pseudo_dataset = pseudo_null_dataset.cast(base_data_casted.collect_schema())
+
+        pseudo_dataset: pl.DataFrame = pl.concat(
+            [base_data_casted, pseudo_dataset]
+        )
+        self.training_logger.info(f'Saving pseudo labeled dataset')
+        
+        (
+            pseudo_dataset
+            .write_parquet(
+            os.path.join(
+                    self.experiment_path_dict['pseudo_labeling'].format(model_type=model_type),
+                    f'data.parquet'
+                )
+            )
+        )
+        self.config_dict['PATH_GOLD_DATA'] = self.experiment_path_dict['pseudo_labeling'].format(model_type=model_type)
+        self.training_logger.info(f"Changed gold path to {self.config_dict['PATH_GOLD_DATA']}")
+            
+    def save_model(self, target: str)->None:            
+        self.save_pickle_model_list(
+            getattr(
+                self, f'model_{target}_list'
+            ), 
+            target,
+        )
+        self.save_progress_list(
+            getattr(
+                self, f'progress_{target}_list'
+            ), 
+            target
+        )
