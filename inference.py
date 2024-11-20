a09ae941e4072feb4dd9d1c974affa939d919163
Davide Stenner <48441753+DavideStenner@users.noreply.github.com>
2024-11-20 09:52:10 +0100

    added ensemble to inference script

--- inference.py
+++ inference.py
@@ -9,8 +9,7 @@ if __name__=='__main__':
     from src.preprocess.pipeline import PreprocessPipeline
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', default='lgb', type=str)
-    parser.add_argument('--all_model', action='store_true')
+    parser.add_argument('--model', default='ensemble', type=str)
     
     args = parser.parse_args()
 
@@ -22,7 +21,40 @@ if __name__=='__main__':
             'test_data.parquet'
         )
     )
-    if (args.model == 'lgb') | (args.all_model):
+    if (args.model == 'ensemble'):
+        from src.model.ensemble.pipeline import EnsemblePipeline
+        
+        config_dict = import_config()    
+        params_model, experiment_name = import_params(model='ensemble')
+        
+        
+        ensemble_pipeline = EnsemblePipeline(
+            experiment_name=experiment_name + '_ensemble', params_ensemble=params_model,
+            config_dict=config_dict
+        )
+        ensemble_pipeline.activate_inference()
+        
+        result_prediction: np.ndarray = ensemble_pipeline.treshold_predict(
+            test_data = test_data
+        )
+        
+        submission = (
+            test_data.select(config_dict['ID_COL'])
+            .to_pandas()
+        )
+        submission[config_dict['COLUMN_INFO']['TARGET']] = result_prediction
+                
+        (
+            submission
+            .to_parquet(
+                os.path.join(
+                    config_dict['PATH_EXPERIMENT'],
+                    experiment_name + "_ensemble",
+                    'submission.parquet'
+                )
+            )
+        )
+    if (args.model == 'lgb'):
         from src.model.lgbm.pipeline import LgbmPipeline
         
         params_model, experiment_name = import_params(model='lgb')
@@ -53,5 +85,3 @@ if __name__=='__main__':
                 )
             )
         )
-    else:
-        raise NotImplementedError
