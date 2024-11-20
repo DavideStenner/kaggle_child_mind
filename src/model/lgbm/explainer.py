df43488751831f284a86ce76472d646ba3cac98a
Davide Stenner <48441753+DavideStenner@users.noreply.github.com>
2024-11-20 22:10:28 +0100

    generalize for different loss also

--- src/model/lgbm/explainer.py
+++ src/model/lgbm/explainer.py
@@ -280,7 +280,7 @@ class LgbmExplainer(LgbmInit):
             best_score = result_by_combination[idx_best_combination]
             best_combination = self.list_treshold_value[idx_best_combination]
             
-            base_cv_score = best_result['best_score']
+            base_cv_score = best_result['all_best_score']['q_kappa']
             increment_score = best_score - base_cv_score
             
             self.training_logger.info(
